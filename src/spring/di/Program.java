402a501457b9666d0bb6747a994de3b9b7104faa
Your Name <cris1945215@gmail.com>
2021-03-13 23:52:04 +0900

    first commit

---
+++ src/spring/di/Program.java
@@ -0,0 +1,14 @@
+package spring.di;
+
+import spring.di.entity.Exam;
+import spring.di.entity.NewLecExam;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Exam exam = new NewLecExam();
+		
+	}
+
+}
---
+++ src/spring/di/entity/NewLecExam.java
@@ -0,0 +1,22 @@
+package spring.di.entity;
+
+public class NewLecExam implements Exam {
+
+	private int kor;
+	private int eng;
+	private int math;
+	private int com;
+	
+	@Override
+	public int total() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int avg() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
